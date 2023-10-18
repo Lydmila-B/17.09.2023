a1be5eeee98a3f0586b47bbb0c4341bb4e1af227
Ludmila <project-070@ya.ru>
2023-10-18 22:38:49 +0700

    Добавила Task46

--- Task01/Program.cs
+++ Task01/Program.cs
@@ -5,15 +5,55 @@
 // a = 9,   b = -3 -> да 
 // a = -3    b = 9 -> нет
 
-Console.WriteLine("Введите первое целое число: ");
-int number1 = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите второе целое число: ");
-int number2 = Convert.ToInt32(Console.ReadLine());
-if (number1 == number2*number2)
+// Console.WriteLine("Введите первое целое число: ");
+// int number1 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите второе целое число: ");
+// int number2 = Convert.ToInt32(Console.ReadLine());
+// if (number1 == number2*number2)
+// {
+//     Console.WriteLine("Первое число является квадратом второго");
+// }
+// else
+// {
+//    Console.WriteLine("Первое число не является квадратом второго"); 
+// }
+
+
+﻿//Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+// m = 3, n = 4.
+//0,5 7 -2 -0,2
+//1 -3,3 8 -9,9
+//8 7,8 -7,1 9
+
+Console.WriteLine("Введите количество строк  ");
+int linesVol = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите количество столбцов");
+int columnsVol = Convert.ToInt32(Console.ReadLine());
+double[,] numbers = new double[linesVol, columnsVol];
+FillArrayRandomNumbers(numbers);
+PrintArray(numbers);
+
+void FillArrayRandomNumbers(double[,] array)
 {
-    Console.WriteLine("Первое число является квадратом второго");
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = Convert.ToDouble(new Random().Next(-100, 100)) / 10;
+        }
+    }
 }
-else
+
+void PrintArray(double[,] array)
 {
-   Console.WriteLine("Первое число не является квадратом второго"); 
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        Console.Write("[ ");
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.Write("]");
+        Console.WriteLine("");
+    }
 }
