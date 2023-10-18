57c27201fbb35a0298ff719cbcebe9a65eb995d0
Ludmila <project-070@ya.ru>
2023-10-18 22:40:22 +0700

    Добавила Task51

---
+++ Task51/Program.cs
@@ -0,0 +1,42 @@
+﻿// Задача 51: Задайте двумерный массив. 
+// Найдите сумму элементов, находящихся на главной диагонали 
+// (с индексами (0,0); (1;1) и т.д. Например, 
+
+// задан массив: 1 4 7 2 5 9 2 3 8 4 2 4 
+// Сумма элементов главной диагонали: 1+9+2 = 12
+
+
+int SumOnDiagonal(int[,] arr) 
+{     
+    int size = arr.GetLength(1);     
+    if (arr.GetLength(0) < arr.GetLength(1)) size = arr.GetLength(0);     
+    int sum = 0;     
+    for (int i = 0; i < size; i++)     
+    {         
+        sum += arr[i, i];     
+    }     
+    return sum; 
+}
+
+int SumOnDiagonalVer2(int[,] arr) 
+{     
+    int sum = 0;     
+    for (int i = 0; i < arr.GetLength(0); i++)     
+    {         
+        for (int j = 0; j < arr.GetLength(1); j++)         
+        {             
+            if (i == j)             
+            {                 
+                sum += arr[i, j];             
+            }         
+        }     
+    }     
+    return sum; 
+}
+
+int[,] rndMatrix = CreatMatrixRandomInt(5, 4, 1, 10); 
+PrintMatrix(rndMatrix);  
+int sumOnDiagonal = SumOnDiagonal(rndMatrix); 
+Console.WriteLine($"Сумма элементов на главной диагонали равна - {sumOnDiagonal}");
+
+
